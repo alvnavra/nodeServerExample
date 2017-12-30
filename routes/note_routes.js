b06b27a5e937e39640a015e77b1cb9d75a6d727b
Rafael Álvarez <alvnavra@gmail.com>
2017-12-30 11:43:59 +0100

    Api con GET,  POST y DELETE

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
     app.post('/notes', (req, res) => {
       console.log('db ==>'+db)
@@ -15,7 +17,6 @@ module.exports = function(app, db) {
   module.exports = function(app, db){
     app.get('/notes/:id', (req, res) =>{
       const id = req.params.id
-      var ObjectID = require('mongodb').ObjectID;
       const details = { '_id': new ObjectID(id) };
       db.collection('notes').findOne(details, (err, item) => {
         if (err) {
@@ -25,4 +26,18 @@ module.exports = function(app, db) {
         } 
       });      
     })
+  }
+
+  module.exports = function(app, db){
+    app.delete('/notes/:id', (req, res) =>{
+      const id = req.params.id
+      const details = { '_id': new ObjectID(id) };
+      db.collection('notes').remove(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send(item);
+        } 
+      });      
+    })    
   }
